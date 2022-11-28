87da3f8b0c9b7a6fc10017207aa8d1c16f357329
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-11-28 20:12:13 -0300

    Adicionar arquivos de projeto.

---
+++ IfElseEleicao/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe sua idade: ");
+            int idade =int.Parse(Console.ReadLine());
+
+            if(idade >= 70)
+            {
+                Console.WriteLine("Você é idoso e seu voto é facultativo");
+            }
+            else if(idade >= 18)
+            {
+                Console.WriteLine("Seu voto é obrigatório!");
+            }
+            else if(idade >= 16)
+            {
+                Console.WriteLine("Você é adolescente e seu voto é facultativo");
+            }
+            else
+            {
+                Console.WriteLine("Você não pode votar");
+            }
+
+
+        }
+    }
+}
